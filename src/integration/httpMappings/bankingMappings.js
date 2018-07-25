6d0afd0fd359c7e57c4b8a9156d5c1ba0b28d100
Andrew Bail <andrew.bail@myob.com>
2018-07-25 11:25:41 +1000

    http mapping to allocate a transaction

--- src/integration/httpMappings/bankingMappings.js
+++ src/integration/httpMappings/bankingMappings.js
@@ -4,5 +4,9 @@ export default {
   [BankingIntents.LOAD_TRANSACTIONS_AND_ACCOUNTS]: {
     method: 'GET',
     path: '/banking/load_transactions_and_accounts'
+  },
+  [BankingIntents.ALLOCATE_ACCOUNT_FOR_TRANSACTION]: {
+    method: 'PUT',
+    path: '/banking/allocate_transaction'
   }
 }
