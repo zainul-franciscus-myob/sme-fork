7b0cc960f41d7002113f2049a9a62a30bdbb1729
Jing Liu <43330528+jing-liu-LJ@users.noreply.github.com>
2018-10-09 11:04:46 +1100

    Add contract for error from BFF (#287)

--- src/integration/httpMapping/handleResponse.js
+++ src/integration/httpMapping/handleResponse.js
@@ -1,7 +1,7 @@
 /**
  * @param {Promise<Response>} fetchedPromise
  * @param {(payload: any) => any} onFulfilled
- * @param {(error: String) => any} onRejected
+ * @param {(Error: Object) => any} onRejected
  */
 const handleResponse = async (fetchedPromise, onFulfilled, onRejected) => {
   /** @type {Response} response */
@@ -11,19 +11,25 @@ const handleResponse = async (fetchedPromise, onFulfilled, onRejected) => {
   try {
     response = await fetchedPromise;
   } catch (error) {
-    onRejected(error.message);
+    onRejected(error);
     return;
   }
 
   if (response.status >= 400) {
-    onRejected(response.statusText);
+    try {
+      const responseBody = await response.json();
+      onRejected({ message: responseBody.message });
+    } catch (error) {
+      onRejected(error);
+      return;
+    }
     return;
   }
 
   try {
     payload = await response.json();
   } catch (error) {
-    onRejected(error.message);
+    onRejected(error);
     return;
   }
 
