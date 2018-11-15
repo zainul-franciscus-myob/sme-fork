67bfce424a7f4e4d40f40b5c9a84bdb7785d9ffa
Jing Liu <jing.liu@myob.com>
2018-11-15 17:45:26 +1100

    make the tests a bit less noise

--- src/generalJournal/__tests__/GeneralJournalDetailReducer.test.js
+++ src/generalJournal/__tests__/GeneralJournalDetailReducer.test.js
@@ -5,33 +5,23 @@ describe('general journal detail reducer', () => {
   const accounts = [{
     id: '123',
     reportingMethod: 'sale',
-    displayName: 'My Sales Account 1',
-    accountType: 'Sales',
     taxCodeId: '123',
-    displayId: '1-123',
   }, {
     id: '456',
     reportingMethod: 'purchase',
-    displayName: 'My Assets Account 1',
-    accountType: 'Assets',
     taxCodeId: '124',
-    displayId: '2-123',
   }];
   const taxCodes = [
     {
-      id: '123', displayName: 'GST', description: 'Goods & Service Tax', displayRate: '10%',
+      id: '123', displayRate: '10%',
     },
     {
-      id: '124', displayName: 'FRE', description: 'GST Free', displayRate: '5%',
+      id: '124', displayRate: '5%',
     },
   ];
   const newLine = {
     accountId: '',
-    debitAmount: '',
-    creditAmount: '',
-    description: '',
     taxCodeId: '',
-    taxAmount: '',
     accounts,
     taxCodes,
   };
@@ -39,41 +29,21 @@ describe('general journal detail reducer', () => {
   describe('UPDATE_GENERAL_JOURNAL_DETAIL_LINE', () => {
     const state = {
       generalJournal: {
-        id: '1',
-        referenceId: 'JE0000002',
-        date: '1533045600000',
         gstReportingMethod: 'sale',
-        isEndOfYearAdjustment: false,
-        isTaxInclusive: false,
-        description: 'Cry havoc',
         lines: [
           {
-            id: '123',
             accountId: '123',
-            debitAmount: '110.00',
-            creditAmount: '',
-            description: 'Cry havoc one',
             taxCodeId: '123',
-            taxAmount: '11.00',
             accounts,
             taxCodes,
           }, {
-            id: '124',
             accountId: '456',
-            debitAmount: '',
-            creditAmount: '110.00',
-            description: 'Cry havoc second',
             taxCodeId: '123',
-            taxAmount: '11.00',
             accounts,
             taxCodes,
           },
         ],
       },
-      newLine,
-      modalType: '',
-      alertMessage: '',
-      isLoading: false,
     };
 
     it('should update gst reporting method when change account for first line', () => {
@@ -86,13 +56,9 @@ describe('general journal detail reducer', () => {
       const newState = generalJournalDetailReducer(state, action);
       expect(newState.generalJournal.gstReportingMethod).toEqual('purchase');
       expect(newState.generalJournal.lines[0]).toEqual({
-        id: '123',
         accountId: '456',
-        debitAmount: '110.00',
-        creditAmount: '',
-        description: 'Cry havoc one',
         taxCodeId: '124',
-        taxAmount: '11',
+        taxAmount: '0',
         accounts,
         taxCodes,
       });
@@ -108,13 +74,9 @@ describe('general journal detail reducer', () => {
       const newState = generalJournalDetailReducer(state, action);
       expect(newState.generalJournal.gstReportingMethod).toEqual('sale');
       expect(newState.generalJournal.lines[1]).toEqual({
-        id: '124',
         accountId: '456',
-        debitAmount: '',
-        creditAmount: '110.00',
-        description: 'Cry havoc second',
         taxCodeId: '124',
-        taxAmount: '11',
+        taxAmount: '0',
         accounts,
         taxCodes,
       });
@@ -124,19 +86,10 @@ describe('general journal detail reducer', () => {
   describe('ADD_GENERAL_JOURNAL_DETAIL_LINE', () => {
     const state = {
       generalJournal: {
-        id: '',
-        referenceId: 'JE0000005',
-        date: '1533045600000',
         gstReportingMethod: '',
-        isEndOfYearAdjustment: false,
-        isTaxInclusive: false,
-        description: '',
         lines: [],
       },
       newLine,
-      modalType: '',
-      alertMessage: '',
-      isLoading: false,
     };
     it('should update the general journal line when add a new line', () => {
       const action = {
@@ -149,11 +102,7 @@ describe('general journal detail reducer', () => {
       expect(newState.generalJournal.gstReportingMethod).toEqual('purchase');
       expect(newState.generalJournal.lines).toEqual([{
         accountId: '456',
-        debitAmount: '',
-        creditAmount: '',
-        description: '',
         taxCodeId: '124',
-        taxAmount: '',
         accounts,
         taxCodes,
       }]);
