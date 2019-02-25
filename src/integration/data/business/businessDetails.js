d6b0968381605f92fde3c62ec10fba257088ad60
Liam Mahony <splinky83@gmail.com>
2019-02-25 13:44:16 +1100

    Read contract for biz deets (#652)
    * Read contract for biz deets

---
+++ src/integration/data/business/businessDetails.js
@@ -0,0 +1,21 @@
+export default {
+  businessDetails: {
+    region: 'AU',
+    organisationName: 'Wayne Corp',
+    abn: '80123123123', // only be used for AU business
+    nzbn: '34', // only be used for NZ business
+    irdNumber: '435302235', // only be used for NZ business
+    gstBranchNumber: '32', // only be used for AU business
+    acn: '123456', // only be used for AU business
+    payeeNumber: 'C3127028', // only be used for AU business
+    phoneNumber: '0212123445',
+    fax: '0212123445',
+    email: 'bruce@waynecorp.com',
+    address: '123 Some Street\nSomewhere VIC 3111',
+    financialYear: '2019',
+    lastMonthInFinancialYear: 'June',
+    accountingPeriods: '13',
+    openingBalanceDate: 'May 2016',
+    serialNumber: '612312344567',
+  },
+};
