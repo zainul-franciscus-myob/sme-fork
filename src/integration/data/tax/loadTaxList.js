bac92e7311fbb9b0be374b8ce40bd0e3eaa57447
Matt Witherow <38416390+matt-witherow@users.noreply.github.com>
2019-02-27 13:38:00 +1100

    Add tax list contract (#662)

---
+++ src/integration/data/tax/loadTaxList.js
@@ -0,0 +1,23 @@
+
+export default {
+  entries: [
+    {
+      id: '1',
+      code: 'GST',
+      description: 'Goods & Services Tax',
+      rate: '10',
+      codeType: 'Goods and Services',
+      collectedAccountName: '2-1220 GST Collected',
+      paidAccountName: '2-1221 GST Paid',
+    },
+    {
+      id: '2',
+      code: 'ABN',
+      description: 'No ABN Witholding',
+      rate: '-47',
+      codeType: 'No ABN/TFN',
+      collectedAccountName: '2-1230 ABN Collected',
+      paidAccountName: '2-1231 ABN Paid',
+    },
+  ],
+};
