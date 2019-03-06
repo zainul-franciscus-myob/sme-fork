5701b45626e6e5878e48d35bc5ba2fa1265d6b58
waanyay <39475527+waanyay@users.noreply.github.com>
2019-03-06 14:33:41 +1100

    683: Quote list contract (#685)

---
+++ src/integration/data/quote/loadQuoteList.js
@@ -0,0 +1,31 @@
+export default {
+  customerId: 'All',
+  customerFilters: [
+    { name: 'All customers', value: 'All' },
+    { name: 'Cameron James', value: '1' },
+    { name: 'John Smith', value: '2' },
+    { name: 'Serena Jaramillo', value: '3' },
+    { name: 'Krzysztof Coleman', value: '4' },
+  ],
+  sortOrder: 'desc',
+  orderBy: 'Number',
+  total: '1,747.00',
+  entries: [
+    {
+      id: '1',
+      referenceId: '00000045',
+      purchaseOrder: 'PO0019374',
+      customer: 'Serena Jaramillo',
+      displayDate: '30/01/2019',
+      displayAmount: '55.00',
+    },
+    {
+      id: '2',
+      referenceId: '00000034',
+      purchaseOrder: 'PO0019394',
+      customer: 'Serena Jaramillo',
+      displayDate: '15/02/2019',
+      displayAmount: '1,692.00',
+    },
+  ],
+};
