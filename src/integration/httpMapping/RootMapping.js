7f18c2818d81bee1bfea51c4afe86eda47f32160
waanyay <39475527+waanyay@users.noreply.github.com>
2019-03-25 16:15:55 +1100

    Add invoice mapping to http integration (#739)

--- src/integration/httpMapping/RootMapping.js
+++ src/integration/httpMapping/RootMapping.js
@@ -3,6 +3,7 @@ import BusinessMapping from './BusinessMapping';
 import ContactMapping from './ContactMapping';
 import GeneralJournalMapping from './GeneralJournalMapping';
 import IncomeAllocationMapping from './IncomeAllocationMapping';
+import InvoiceMapping from './InvoiceMapping';
 import NavigationMapping from './NavigationMapping';
 import QuoteMapping from './QuoteMapping';
 import ReceiveMoneyMapping from './ReceiveMoneyMapping';
@@ -18,6 +19,7 @@ const RootMapping = Object.freeze({
   ...IncomeAllocationMapping,
   ...NavigationMapping,
   ...QuoteMapping,
+  ...InvoiceMapping,
   ...ReceiveMoneyMapping,
   ...SpendMoneyMapping,
   ...TransactionListMapping,
