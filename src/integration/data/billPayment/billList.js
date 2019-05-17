38d641f7a592fca4a7661e02674b119e578ee6ae
Jack Dinh <jdinhify@gmail.com>
2019-05-17 10:27:42 +1000

    📚 contract for bill payment (#882)

---
+++ src/integration/data/billPayment/billList.js
@@ -0,0 +1,14 @@
+const billList = {
+  entries: [
+    {
+      id: '123',
+      billNumber: '0000023',
+      status: 'Open',
+      date: '27/03/2019',
+      billAmount: '250.05',
+      discount: '',
+    },
+  ],
+};
+
+export default billList;
