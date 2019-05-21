3c4f6af61a881906d1d402ba5bffc45299cb63c6
Tony Tang <tsukiy0@users.noreply.github.com>
2019-05-21 13:44:42 +1000

    Add contract for invoice payment

---
+++ src/integration/data/invoicePayment/invoiceList.js
@@ -0,0 +1,14 @@
+const invoiceList = {
+  entries: [
+    {
+      id: '123',
+      invoiceNumber: '0000023',
+      status: 'Unpaid',
+      date: '27/03/2019',
+      invoiceAmount: '250.05',
+      discountAmount: '',
+    },
+  ],
+};
+
+export default invoiceList;
