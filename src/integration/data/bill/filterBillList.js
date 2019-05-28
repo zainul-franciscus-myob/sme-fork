9a3cd14ea6dd77aaa7908216e980aafb48039bfa
Uditha Jayasinghe <uditha.jayasinghe@myob.com>
2019-05-28 10:31:06 +1000

    Added bills list ui contract (#906)

---
+++ src/integration/data/bill/filterBillList.js
@@ -0,0 +1,28 @@
+export default {
+  total: '$600.00',
+  totalDue: '$56.34',
+  entries: [
+    {
+      id: '1',
+      billNumber: '00000034',
+      invoiceNumber: '126',
+      supplier: 'Footloose Dance Studio',
+      dateIssued: '01/03/2019',
+      billAmount: '100.00',
+      balanceDue: '56.34',
+      status: 'Open',
+      dateClosed: '',
+    },
+    {
+      id: '2',
+      billNumber: '00000035',
+      invoiceNumber: '125',
+      supplier: 'Dog Foods',
+      dateIssued: '01/03/2019',
+      billAmount: '500.00',
+      balanceDue: '0.00',
+      status: 'Closed',
+      dateClosed: '01/04/2019',
+    },
+  ],
+};
