211bb63bd4dac4de9a2a8e311b6a6d217c069f45
Tony Tang <tsukiy0@users.noreply.github.com>
2019-07-31 10:14:36 +1000

    Add 200ms sleep to memory integration requests (#1074)

--- src/integration/createMemoryIntegration.js
+++ src/integration/createMemoryIntegration.js
@@ -1,25 +1,33 @@
 import RootMapping from './memoryMapping/RootMapping';
 
+const sleep = duration => () => new Promise((resolve) => {
+  setTimeout(resolve, duration);
+});
+
 const createMemoryIntegration = () => ({
   read: async ({
     intent, params, onSuccess, onFailure, urlParams,
   }) => {
     const integrationFunction = RootMapping[intent];
-    Promise.resolve().then(() => {
-      integrationFunction({
-        urlParams, params, onSuccess, onFailure,
+    Promise.resolve()
+      .then(sleep(200))
+      .then(() => {
+        integrationFunction({
+          urlParams, params, onSuccess, onFailure,
+        });
       });
-    });
   },
   write: async ({
     intent, params, content, onSuccess, onFailure,
   }) => {
     const integrationFunction = RootMapping[intent];
-    Promise.resolve().then(() => {
-      integrationFunction({
-        params, content, onSuccess, onFailure,
+    Promise.resolve()
+      .then(sleep(200))
+      .then(() => {
+        integrationFunction({
+          params, content, onSuccess, onFailure,
+        });
       });
-    });
   },
 });
 
