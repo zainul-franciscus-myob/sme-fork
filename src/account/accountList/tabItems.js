4383b30d0aa638f8905e703feb6cc48c0c9c4cee
Waanya Yoosthaporn <39475527+waanyay@users.noreply.github.com>
2019-10-15 14:33:02 +1100

    Update coa tab name change (#1249)

--- src/account/accountList/tabItems.js
+++ src/account/accountList/tabItems.js
@@ -14,7 +14,7 @@ export const tabItems = [
   { id: tabIds.all, label: 'All accounts' },
   { id: tabIds.asset, label: 'Assets' },
   { id: tabIds.liability, label: 'Liabilities' },
-  { id: tabIds.equity, label: 'Equities' },
+  { id: tabIds.equity, label: 'Equity' },
   { id: tabIds.income, label: 'Income' },
   { id: tabIds.costOfSales, label: 'Cost of sales' },
   { id: tabIds.expense, label: 'Expenses' },
