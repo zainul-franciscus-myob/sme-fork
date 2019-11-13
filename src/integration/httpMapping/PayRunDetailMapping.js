1a2e73e91d5924f237d9cc54aea8c09b936350a8
Andrew Kemm <56860088+andrew-kemm-at-myob@users.noreply.github.com>
2019-11-13 14:26:40 +1100

    Update the pay run detail BFF endpoint to match BFF changes (#1344)

--- src/integration/httpMapping/PayRunDetailMapping.js
+++ src/integration/httpMapping/PayRunDetailMapping.js
@@ -3,7 +3,7 @@ import { LOAD_PAY_RUN_DETAILS } from '../../payRun/payRunDetail/payRunDetailInte
 const PayRunDetailMapping = {
   [LOAD_PAY_RUN_DETAILS]: {
     method: 'GET',
-    getPath: ({ businessId, payRunId }) => `/${businessId}/payrun/${payRunId}/load_pay_run_details`,
+    getPath: ({ businessId, payRunId }) => `/${businessId}/payrun/load_pay_run_detail/${payRunId}`,
   },
 };
 
