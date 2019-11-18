bbf8b61a54c681e2ce0c046167fc0649406ed276
CHENG LI <rivneg@outlook.com>
2019-11-18 14:45:19 +1100

    Make bill list table header and filter options sticky (#1371)

--- src/bill/billList/components/BillListTable.js
+++ src/bill/billList/components/BillListTable.js
@@ -1,68 +1,25 @@
 import {
-  Button, HeaderSort, Icons, PageState, Table,
+  Button, Icons, PageState,
 } from '@myob/myob-widgets';
 import { connect } from 'react-redux';
 import React from 'react';
 
 import {
-  getIsTableEmpty, getIsTableLoading, getOrder, getTableBodyState,
+  getIsTableEmpty, getIsTableLoading, getTableBodyState,
 } from '../billListSelectors';
 import BillListTableBody from './BillListTableBody';
+import StickyTableBody from '../../../components/StickyTable/StickyTableBody';
 import TableBodyType from '../TableBodyType';
-import TableView from '../../../components/TableView/TableView';
 import billsEmptyStateImage from './bills-empty-state.svg';
 import noResultStateImage from './no-results-state.svg';
-import widthConfig from './widthConfig';
-
-const tableConfig = {
-  dateIssued: { columnName: 'Date', valign: 'top' },
-  number: { columnName: 'Bill no', valign: 'top' },
-  supplier: { columnName: 'Supplier', valign: 'top' },
-  invoiceNumber: { columnName: 'Supplier inv no', valign: 'top' },
-  billAmount: { columnName: 'Amount ($)', valign: 'top', align: 'right' },
-  balanceDue: { columnName: 'Balance due ($)', valign: 'top', align: 'right' },
-  dateDue: { columnName: 'Due date', valign: 'top' },
-  attachment: { columnName: 'Attachment', valign: 'top' },
-  status: { columnName: 'Status', valign: 'top' },
-};
-
-const HeaderItem = ({
-  config, sortName, activeSort, onSort,
-}) => (
-  <Table.HeaderItem {...config}>
-    <HeaderSort
-      title={config.columnName}
-      sortName={sortName}
-      activeSort={activeSort}
-      onSort={onSort}
-    />
-  </Table.HeaderItem>
-);
 
 const BillListTable = ({
   isTableEmpty,
   isTableLoading,
   tableBodyState,
-  onSort,
-  order,
   onCreateButtonClick,
+  tableConfig,
 }) => {
-  const header = (
-    <Table.Header>
-      <HeaderItem config={tableConfig.dateIssued} sortName="DateOccurred" activeSort={order} onSort={onSort} />
-      <HeaderItem config={tableConfig.number} sortName="DisplayId" activeSort={order} onSort={onSort} />
-      <HeaderItem config={tableConfig.supplier} sortName="SupplierName" activeSort={order} onSort={onSort} />
-      <HeaderItem config={tableConfig.invoiceNumber} sortName="CustomerPurchaseOrderIdentifier" activeSort={order} onSort={onSort} />
-      <HeaderItem config={tableConfig.billAmount} sortName="Amount" activeSort={order} onSort={onSort} />
-      <HeaderItem config={tableConfig.balanceDue} sortName="BalanceDue" activeSort={order} onSort={onSort} />
-      <HeaderItem config={tableConfig.dateDue} sortName="DateDue" activeSort={order} onSort={onSort} />
-      <Table.HeaderItem {...tableConfig.attachment}>
-        { tableConfig.attachment.columnName }
-      </Table.HeaderItem>
-      <HeaderItem config={tableConfig.status} sortName="Status" activeSort={order} onSort={onSort} />
-    </Table.Header>
-  );
-
   const noResultsPageState = (
     <PageState
       title="No bills found :("
@@ -80,32 +37,25 @@ const BillListTable = ({
     />
   );
 
-  const tableView = <BillListTableBody tableConfig={tableConfig} />;
-
   const emptyView = {
     [TableBodyType.EMPTY]: emptyPageState,
     [TableBodyType.NO_RESULTS]: noResultsPageState,
   }[tableBodyState];
 
-  const responsiveWidths = widthConfig(tableConfig);
-
   return (
-    <TableView
+    <StickyTableBody
       isLoading={isTableLoading}
       isEmpty={isTableEmpty}
       emptyView={emptyView}
-      header={header}
-      responsiveWidths={responsiveWidths}
     >
-      { tableView }
-    </TableView>
+      <BillListTableBody tableConfig={tableConfig} />
+    </StickyTableBody>
   );
 };
 
 const mapStateToProps = state => ({
   isTableLoading: getIsTableLoading(state),
   isTableEmpty: getIsTableEmpty(state),
-  order: getOrder(state),
   tableBodyState: getTableBodyState(state),
 });
 
--- src/bill/billList/components/BillListView.js
+++ src/bill/billList/components/BillListView.js
@@ -1,19 +1,46 @@
 import {
-  Alert, Button, PageHead, StandardTemplate,
+  Alert, Button, HeaderSort, PageHead, StandardTemplate, Table,
 } from '@myob/myob-widgets';
 import { connect } from 'react-redux';
 import React from 'react';
 
-import { getAlert, getIsLoading } from '../billListSelectors';
+import { getAlert, getIsLoading, getOrder } from '../billListSelectors';
 import BillListFilterOptions from './BillListFilterOptions';
 import BillListTable from './BillListTable';
 import PageView from '../../../components/PageView/PageView';
 import style from './BillListView.module.css';
+import widthConfig from './widthConfig';
+
+const tableConfig = {
+  dateIssued: { columnName: 'Date', valign: 'top' },
+  number: { columnName: 'Bill no', valign: 'top' },
+  supplier: { columnName: 'Supplier', valign: 'top' },
+  invoiceNumber: { columnName: 'Supplier inv no', valign: 'top' },
+  billAmount: { columnName: 'Amount ($)', valign: 'top', align: 'right' },
+  balanceDue: { columnName: 'Balance due ($)', valign: 'top', align: 'right' },
+  dateDue: { columnName: 'Due date', valign: 'top' },
+  attachment: { columnName: 'Attachment', valign: 'top' },
+  status: { columnName: 'Status', valign: 'top' },
+};
+
+const HeaderItem = ({
+  config, sortName, activeSort, onSort,
+}) => (
+  <Table.HeaderItem {...config}>
+    <HeaderSort
+      title={config.columnName}
+      sortName={sortName}
+      activeSort={activeSort}
+      onSort={onSort}
+    />
+  </Table.HeaderItem>
+);
 
 const BillListView = (props) => {
   const {
     isLoading,
     alert,
+    order,
     onDismissAlert,
     onUpdateFilters,
     onApplyFilter,
@@ -40,11 +67,37 @@ const BillListView = (props) => {
     </PageHead>
   );
 
+  const responsiveWidths = widthConfig(tableConfig);
+
+  const tableHeader = (
+    <Table responsiveWidths={responsiveWidths}>
+      <Table.Header>
+        <HeaderItem config={tableConfig.dateIssued} sortName="DateOccurred" activeSort={order} onSort={onSort} />
+        <HeaderItem config={tableConfig.number} sortName="DisplayId" activeSort={order} onSort={onSort} />
+        <HeaderItem config={tableConfig.supplier} sortName="SupplierName" activeSort={order} onSort={onSort} />
+        <HeaderItem config={tableConfig.invoiceNumber} sortName="CustomerPurchaseOrderIdentifier" activeSort={order} onSort={onSort} />
+        <HeaderItem config={tableConfig.billAmount} sortName="Amount" activeSort={order} onSort={onSort} />
+        <HeaderItem config={tableConfig.balanceDue} sortName="BalanceDue" activeSort={order} onSort={onSort} />
+        <HeaderItem config={tableConfig.dateDue} sortName="DateDue" activeSort={order} onSort={onSort} />
+        <Table.HeaderItem {...tableConfig.attachment}>
+          { tableConfig.attachment.columnName }
+        </Table.HeaderItem>
+        <HeaderItem config={tableConfig.status} sortName="Status" activeSort={order} onSort={onSort} />
+      </Table.Header>
+    </Table>
+  );
+
   const billListView = (
-    <StandardTemplate alert={alertComponent} sticky="none" pageHead={pageHead} filterBar={filterBar}>
+    <StandardTemplate
+      alert={alertComponent}
+      sticky="all"
+      pageHead={pageHead}
+      filterBar={filterBar}
+      tableHeader={tableHeader}
+    >
       <div className={style.list}>
         <BillListTable
-          onSort={onSort}
+          tableConfig={tableConfig}
           onCreateButtonClick={onCreateButtonClick}
         />
       </div>
@@ -57,6 +110,7 @@ const BillListView = (props) => {
 const mapStateToProps = state => ({
   alert: getAlert(state),
   isLoading: getIsLoading(state),
+  order: getOrder(state),
 });
 
 export default connect(mapStateToProps)(BillListView);
