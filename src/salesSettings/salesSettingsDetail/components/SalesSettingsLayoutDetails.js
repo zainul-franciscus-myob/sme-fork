0a1971596a9d5c732399bd469bcbae81746fdc37
Renae Heinrich <47548287+renae-heinrich@users.noreply.github.com>
2019-11-26 12:49:28 +1100

    Revert "Item and service layout setting (#1441)" (#1444)
    This reverts commit 58d889f2dffed55f7831b0f80a72bae1e065fce3.

--- src/salesSettings/salesSettingsDetail/components/SalesSettingsLayoutDetails.js
+++ src/salesSettings/salesSettingsDetail/components/SalesSettingsLayoutDetails.js
@@ -1,13 +1,15 @@
 import {
-  Card, FieldGroup, Select,
+  Card, FieldGroup, ReadOnly, Select,
 } from '@myob/myob-widgets';
+import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import React from 'react';
 
 import {
-  getLayout,
+  getLayout, getLayoutDescription,
   getTabData,
 } from '../SalesSettingsDetailSelectors';
+import styles from './SalesSettingsLayoutDetails.module.css';
 
 const onInputChange = handler => e => handler({
   key: e.target.name,
@@ -18,12 +20,16 @@ const SalesSettingsLayoutDetails = ({
   salesSettings,
   onUpdateSalesSettingsItem,
   layout,
+  layoutDescription,
 }) => (
   <Card>
     <FieldGroup label="Layout">
+      <p>
+        Choose a layout to determine what information shows on your invoices and quotes.
+      </p>
       <Select
         name="defaultSaleLayout"
-        label="Field Layout"
+        label="Layout"
         value={salesSettings.defaultSaleLayout}
         onChange={onInputChange(onUpdateSalesSettingsItem)}
       >
@@ -31,13 +37,29 @@ const SalesSettingsLayoutDetails = ({
           <Select.Option key={value} value={value} label={name} />
         ))}
       </Select>
+      <ReadOnly
+        name="description"
+        label="Description"
+        hideLabel
+        className={styles.layoutDescription}
+      >
+        { layoutDescription }
+      </ReadOnly>
     </FieldGroup>
   </Card>
 );
 
+SalesSettingsLayoutDetails.propTypes = {
+  salesSettings: PropTypes.shape({}).isRequired,
+  layout: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  onUpdateSalesSettingsItem: PropTypes.func.isRequired,
+  layoutDescription: PropTypes.string.isRequired,
+};
+
 const mapStateToProps = state => ({
   salesSettings: getTabData(state),
   layout: getLayout(state),
+  layoutDescription: getLayoutDescription(state),
 });
 
 export default connect(mapStateToProps)(SalesSettingsLayoutDetails);
