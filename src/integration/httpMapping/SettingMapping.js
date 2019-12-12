ef57d9c1a71490f82e49f80f7bac57ed7195f102
Matt Ensor <mattyensor@gmail.com>
2019-12-12 17:25:47 +1100

    update settings patches to puts (#1575)

--- src/integration/httpMapping/SettingMapping.js
+++ src/integration/httpMapping/SettingMapping.js
@@ -2,7 +2,7 @@ import { LOAD_SETTINGS, SAVE_SETTINGS } from '../../root/rootIntents';
 
 const SettingMapping = {
   [LOAD_SETTINGS]: { method: 'GET', getPath: ({ businessId }) => `/${businessId}/settings` },
-  [SAVE_SETTINGS]: { method: 'PATCH', getPath: ({ businessId }) => `/${businessId}/settings` },
+  [SAVE_SETTINGS]: { method: 'PUT', getPath: ({ businessId }) => `/${businessId}/settings` },
 };
 
 export default SettingMapping;
