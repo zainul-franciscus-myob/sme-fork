fdf8bd016aecaee6a019f8492f21e8d3ccb32208
shohremansourimyob <56813454+shohremansourimyob@users.noreply.github.com>
2020-01-17 10:47:36 +1100

    Fix formatting issue on draft pay run dates (#1842)
    Co-authored-by: Cecily Shen <56851103+cecilyatmyob@users.noreply.github.com>

--- src/payRun/payRunCreate/startPayRun/components/ExistingPayRunModal.js
+++ src/payRun/payRunCreate/startPayRun/components/ExistingPayRunModal.js
@@ -14,6 +14,7 @@ import {
   getPayPeriodEnd,
   getPayPeriodStart,
 } from '../StartPayRunSelectors';
+import formatSlashDate from '../../../../common/valueFormatters/formatDate/formatSlashDate';
 import styles from './StartPayRunView.module.css';
 
 const ExistingPayRunModal = ({
@@ -36,9 +37,9 @@ const ExistingPayRunModal = ({
       </Alert>
 
       <FormHorizontal>
-        <ReadOnly name="PayPeriodStart" label="Pay period start">{payPeriodStart}</ReadOnly>
-        <ReadOnly name="PayPeriodEnd" label="Pay period end">{payPeriodEnd}</ReadOnly>
-        <ReadOnly name="DateOfPayment" label="Date of payment">{dateOfPayment}</ReadOnly>
+        <ReadOnly name="PayPeriodStart" label="Pay period start">{formatSlashDate(payPeriodStart)}</ReadOnly>
+        <ReadOnly name="PayPeriodEnd" label="Pay period end">{formatSlashDate(payPeriodEnd)}</ReadOnly>
+        <ReadOnly name="DateOfPayment" label="Date of payment">{formatSlashDate(dateOfPayment)}</ReadOnly>
       </FormHorizontal>
 
     </Modal.Body>
