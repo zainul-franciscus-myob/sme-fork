3701c10403bcc6b95af7e2b8b5f36b4c530199ff
Ryan-Xiao <53714749+Ryan-Xiao@users.noreply.github.com>
2020-01-28 13:07:22 +1100

    Fix mouse pointer jumps bug (#1927)
    * fix one
    
    * Quote: use amount and discount for AmountInput
    
    * Bill: use amount and discount for AmountInput
    
    * Quote: service table row use amount and discount for AmountInput
    
    * Invoice: Item table use amount and discount for AmountInput
    
    * Invoice: service table use amount for AmountInput
    
    * Bill: service table use amount for AmountInput
    
    * stop setRawValue from trigger onChange
    
    * Revert changes on modules
    
    * Update comment

--- src/components/autoFormatter/AutoFormatterCore/AutoFormatterWithMessage.js
+++ src/components/autoFormatter/AutoFormatterCore/AutoFormatterWithMessage.js
@@ -1,8 +1,20 @@
 import Cleave from 'cleave.js/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 
 import FieldMessagePopup from '../../FieldMessagePopup/FieldMessagePopup';
+
+/**
+ * #PROBLEM
+ * If we enter value after a comma, for exmple update '1,000.00' to '12,000.00'
+ * mouse pointer jumps to the end
+ * #SOLUTION
+ * when we call cleave.setRawValue, it update the value properly without mouse jumping
+ * however it will trigger onChange event which only return value and rawValue
+ * we don't need setRawValue to trigger onChange event
+ * as setRawValue is trigger by onChange event
+*/
+const isActuallyAnEvent = e => Boolean(e.target.name);
 
 const Input = ({
   label,
@@ -16,29 +28,48 @@ const Input = ({
   infoBody,
   className,
   requiredLabel,
+  value,
+  onChange,
   ...inputBoxProps
-}) => (
-  <FieldMessagePopup
-    label={label}
-    id={id}
-    hideLabel={hideLabel}
-    labelAccessory={labelAccessory}
-    errorMessage={errorMessage}
-    errorMessageInline={errorMessageInline}
-    warningBody={warningBody}
-    infoBody={infoBody}
-    requiredLabel={requiredLabel}
-    renderField={({ errorId, ...props }) => (
-      <Cleave
-        {...props}
-        {...inputBoxProps}
-        className={classnames('form-control', className, {
-          'text-align-center': textAlign === 'center',
-          'text-align-right': textAlign === 'right',
-        })}
-      />
-    )}
-  />
-);
+}) => {
+  const [cleave, setCleave] = useState(null);
+
+  useEffect(() => {
+    if (!cleave || !value) return;
+    cleave.setRawValue(value);
+  }, [cleave, value]);
+
+  const cleaveOnChangeHandler = (e) => {
+    if (isActuallyAnEvent(e)) {
+      onChange(e);
+    }
+  };
+
+  return (
+    <FieldMessagePopup
+      label={label}
+      id={id}
+      hideLabel={hideLabel}
+      labelAccessory={labelAccessory}
+      errorMessage={errorMessage}
+      errorMessageInline={errorMessageInline}
+      warningBody={warningBody}
+      infoBody={infoBody}
+      requiredLabel={requiredLabel}
+      renderField={({ errorId, ...props }) => (
+        <Cleave
+          {...props}
+          {...inputBoxProps}
+          onChange={cleaveOnChangeHandler}
+          onInit={setCleave}
+          className={classnames('form-control', className, {
+            'text-align-center': textAlign === 'center',
+            'text-align-right': textAlign === 'right',
+          })}
+        />
+      )}
+    />
+  );
+};
 
 export default Input;
