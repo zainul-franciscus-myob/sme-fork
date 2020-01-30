132f6dd58af0df2995fd0fd9fd67e423a140de15
CHENG LI <rivneg@outlook.com>
2020-01-30 14:56:31 +1100

    Fix issues of tax calculator wrapper (#1965)
    * Fix withholding tax rate and map rounding method
    
    * Add calculation methods map
    
    * Build child tax code collection
    
    * Fix test
    
    * Fix wrong enum mapping

--- src/common/taxCalculator/buildTaxCodesMetadata.js
+++ src/common/taxCalculator/buildTaxCodesMetadata.js
@@ -6,43 +6,65 @@ const CodeTypes = {
   SalesTax: 2,
   GST_VAT: 3,
   InputTaxed: 4,
-  LuxuryCarTax: 5,
-  WithholdingsTax: 6,
-  NoABN_TFN: 7,
+  QST: 5,
+  LuxuryCarTax: 6,
+  WithholdingsTax: 7,
+  NoABN_TFN: 8,
 };
 
+const RoundingMethods = {
+  None: 0,
+  RoundDownNearestDollar: 1,
+  RoundMoney: 2,
+  Truncate: 3,
+};
+
+const CalculationMethods = {
+  None: 0,
+  BasicConsolidated: 1,
+  BasicProportional: 2,
+  ConsolidatedPostEvent: 3,
+  ConsolidatedThreshold: 4,
+  ProportionalPostEvent: 5,
+  ProportionalThreshold: 6,
+};
+
+const buildTaxCodeMetadata = ({
+  id,
+  displayName,
+  codeType,
+  rate = 0,
+  threshold,
+  childrenCalculationCollection = [],
+  calculationMethod,
+  roundingMethod,
+  collectedBehaviour,
+  payedBehaviour,
+  isWithholding,
+  thresholdRate,
+  includeInGstReturn,
+}) => ({
+  Id: id,
+  Code: displayName,
+  CodeType: CodeTypes[codeType],
+  Rate: Decimal(isWithholding ? Math.abs(Number(rate)) : rate),
+  Threshold: Decimal(threshold),
+  ChildrenCalculationCollection: childrenCalculationCollection.map(
+    childTaxCode => buildTaxCodeMetadata(childTaxCode),
+  ),
+  CalculationMethod: CalculationMethods[calculationMethod],
+  RoundingMethod: RoundingMethods[roundingMethod],
+  CollectedBehaviour: Number(collectedBehaviour),
+  PayedBehaviour: Number(payedBehaviour),
+  IsWithholding: isWithholding,
+  ThresholdRate: Decimal(thresholdRate),
+  IncludeInGstReturn: includeInGstReturn,
+});
+
 const buildTaxCodesMetadata = taxCodes => (
-  taxCodes.reduce((results, {
-    id,
-    displayName,
-    codeType,
-    rate,
-    threshold,
-    childrenCalculationCollection,
-    calculationMethod,
-    roundingMethod,
-    collectedBehaviour,
-    payedBehaviour,
-    isWithholding,
-    thresholdRate,
-    includeInGstReturn,
-  }) => ({
+  taxCodes.reduce((results, taxCode) => ({
     ...results,
-    [id]: {
-      Id: id,
-      Code: displayName,
-      CodeType: CodeTypes[codeType],
-      Rate: Decimal(rate),
-      Threshold: Decimal(threshold),
-      ChildrenCalculationCollection: childrenCalculationCollection,
-      CalculationMethod: calculationMethod,
-      RoundingMethod: roundingMethod,
-      CollectedBehaviour: Number(collectedBehaviour),
-      PayedBehaviour: Number(payedBehaviour),
-      IsWithholding: isWithholding,
-      ThresholdRate: Decimal(thresholdRate),
-      IncludeInGstReturn: includeInGstReturn,
-    },
+    [taxCode.id]: buildTaxCodeMetadata(taxCode),
   }), {})
 );
 
