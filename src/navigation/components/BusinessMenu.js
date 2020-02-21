ec3fff7a581c79c1a2f6672f5d60fd2a433168af
Craig Sullivan <hi@craigs.io>
2020-02-21 16:18:41 +1100

    Re-enable change plan menu (#2176)

--- src/navigation/components/BusinessMenu.js
+++ src/navigation/components/BusinessMenu.js
@@ -83,7 +83,7 @@ const getItems = ({
   getDisabledMenuLink('my.MYOB account'),
   urls.paymentDetail && getMenuLink(urls.paymentDetail, 'Payment details', onMenuLinkClick),
   onSubscribeNowClick && getMenuLink('', 'Subscribe now', onSubscribeNowClick),
-  false && onChangePlanClick && getMenuLink('', 'Change plan', onChangePlanClick),
+  onChangePlanClick && getMenuLink('', 'Change plan', onChangePlanClick),
   <Navigation.Separator key="separator-links" />,
   serialNumber && <UnlinkedMenuLink label={`Serial number: ${serialNumber}`} />,
   getMenuLinkWithIcon('', 'Logout', <Icons.SignOut />, onLogoutLinkClick),
