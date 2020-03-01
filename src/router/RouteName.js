39b2cc8dcc8d5581dcc22e9f783bddfa447286fb
Franklin Ross <fhross@gmail.com>
2020-03-02 10:22:09 +1100

    Remove bad character from route name (#2248)

--- src/router/RouteName.js
+++ src/router/RouteName.js
@@ -79,7 +79,7 @@ const RouteName = {
   REPORTS_EXCEPTION: 'reportsException',
   REPORTS_PACK_BUILDER: 'reportsPackBuilder',
   REPORTS_PDF_STYLE_TEMPLATES: 'reportsPdfStyleTemplates',
-  STP_GET_STARTED: 'stp/stpGetStarted',
+  STP_GET_STARTED: 'stp/stpGetStarted',
   STP_ERRORS: 'stp/stpErrors',
   STP_SETUP: 'stp/stpSetup',
   STP_REPORTING_CENTRE: 'stp/reportingCentre',
