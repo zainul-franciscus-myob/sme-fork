70b145b2f2b96cad3c7438cc2fb86b003c65f2b2
Geoff Speirs <38023796+myob-geoff-speirs@users.noreply.github.com>
2020-03-05 11:51:03 +1100

    Bugfix/customer reminders (#2312)
    * fixing uid for remidners link

--- src/modules/contact/contactDetail/__tests__/ContactDetailSelectors.test.js
+++ src/modules/contact/contactDetail/__tests__/ContactDetailSelectors.test.js
@@ -10,7 +10,9 @@ describe('contactDetailSelectors', () => {
           url: 'https://justin.com/preferences/customer',
         },
         businessId: '1234',
-        uid: 'e312-e312-e312-e312',
+        contact: {
+          uid: 'e312-e312-e312-e312',
+        },
       };
 
       const expected = 'https://justin.com/preferences/customer?consumer=ARL&origin=Customer&cfid=1234&id=e312-e312-e312-e312';
