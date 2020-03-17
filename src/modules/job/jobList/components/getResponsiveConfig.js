a6d6a6be283211ef911db0d51b973c96e26f33fd
akshaybanaik <54254995+akshaybanaik@users.noreply.github.com>
2020-03-17 15:01:21 +1100

    Job list nesting responsive (#2401)
    * Indentation responsive fixes
    
    * Remove status column elipsis
    
    * Fix memory mapping data type

--- src/modules/job/jobList/components/getResponsiveConfig.js
+++ src/modules/job/jobList/components/getResponsiveConfig.js
@@ -2,12 +2,12 @@ export const getResponsiveConfig = tableConfig => ([
   {
     'min-width': '768px',
     config: [
-      { columnName: tableConfig.number.columnName, styles: { width: '8rem' } },
-      { columnName: tableConfig.isActive.columnName, styles: { width: '7rem' } },
-      { columnName: tableConfig.income.columnName, styles: { width: '13rem' } },
-      { columnName: tableConfig.cost.columnName, styles: { width: '13rem' } },
-      { columnName: tableConfig.expenses.columnName, styles: { width: '13rem' } },
-      { columnName: tableConfig.netProfit.columnName, styles: { width: '13.5rem' } },
+      { columnName: tableConfig.number.columnName, styles: { width: '6.6rem' } },
+      { columnName: tableConfig.isActive.columnName, styles: { width: '6.6rem' } },
+      { columnName: tableConfig.income.columnName, styles: { width: '12.6rem' } },
+      { columnName: tableConfig.cost.columnName, styles: { width: '12.6rem' } },
+      { columnName: tableConfig.expenses.columnName, styles: { width: '12.6rem' } },
+      { columnName: tableConfig.netProfit.columnName, styles: { width: '13.1rem' } },
     ],
   },
   {
@@ -25,7 +25,7 @@ export const getResponsiveConfig = tableConfig => ([
     'min-width': '900px',
     config: [
       { columnName: tableConfig.number.columnName, styles: { width: '9rem' } },
-      { columnName: tableConfig.isActive.columnName, styles: { width: '7rem' } },
+      { columnName: tableConfig.isActive.columnName, styles: { width: '7.5rem' } },
       { columnName: tableConfig.income.columnName, styles: { width: '13rem' } },
       { columnName: tableConfig.cost.columnName, styles: { width: '13rem' } },
       { columnName: tableConfig.expenses.columnName, styles: { width: '13rem' } },
@@ -35,7 +35,7 @@ export const getResponsiveConfig = tableConfig => ([
   {
     'min-width': '1000px',
     config: [
-      { columnName: tableConfig.number.columnName, styles: { width: '14rem' } },
+      { columnName: tableConfig.number.columnName, styles: { width: '12rem' } },
       { columnName: tableConfig.isActive.columnName, styles: { width: '8rem' } },
       { columnName: tableConfig.income.columnName, styles: { width: '14rem' } },
       { columnName: tableConfig.cost.columnName, styles: { width: '14rem' } },
@@ -47,10 +47,11 @@ export const getResponsiveConfig = tableConfig => ([
     'min-width': '1200px',
     config: [
       { columnName: tableConfig.number.columnName, styles: { width: '16rem' } },
-      { columnName: tableConfig.income.columnName, styles: { width: '17rem' } },
-      { columnName: tableConfig.cost.columnName, styles: { width: '17rem' } },
-      { columnName: tableConfig.expenses.columnName, styles: { width: '17rem' } },
-      { columnName: tableConfig.netProfit.columnName, styles: { width: '17rem' } },
+      { columnName: tableConfig.isActive.columnName, styles: { width: '9rem' } },
+      { columnName: tableConfig.income.columnName, styles: { width: '16rem' } },
+      { columnName: tableConfig.cost.columnName, styles: { width: '16rem' } },
+      { columnName: tableConfig.expenses.columnName, styles: { width: '16rem' } },
+      { columnName: tableConfig.netProfit.columnName, styles: { width: '16rem' } },
     ],
   },
 ]);
