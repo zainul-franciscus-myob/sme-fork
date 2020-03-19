e1d86354ee6b352ccef34e263db7704a8cd68dc2
Justin Feng <justin.feng@myob.com>
2020-03-18 23:39:03 -0500

    Add defensive code to handle no body (#2439)

--- src/stopResizeAnimation.js
+++ src/stopResizeAnimation.js
@@ -3,10 +3,12 @@
 export default () => {
   let resizeTimer;
   window.addEventListener('resize', () => {
-    document.body.classList.add('resize-animation-stopper');
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-      document.body.classList.remove('resize-animation-stopper');
-    }, 50);
+    if (document.body) {
+      document.body.classList.add('resize-animation-stopper');
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+      }, 50);
+    }
   });
 };
