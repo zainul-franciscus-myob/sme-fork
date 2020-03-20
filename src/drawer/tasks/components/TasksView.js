dbd2e7eb9f0b1eebe89d4f27ec89ec1b0e1200e8
Thiago Campezzi <campezzi@gmail.com>
2020-03-20 14:22:56 +1100

    Add bell icon to feed drawer (#2444)

--- src/drawer/tasks/components/TasksView.js
+++ src/drawer/tasks/components/TasksView.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { getIsActive, getIsLoading } from '../TasksSelectors';
 import Onboarding from './Onboarding';
 import PageView from '../../../components/PageView/PageView';
+import TasksIcon from '../../../navigation/components/images/TasksIcon.svg';
 import Welcome from './Welcome';
 import asideHeaderStyles from '../../AsideHeader.module.css';
 import emptyStateImage from './assets/icon-tasks-empty-state.svg';
@@ -46,8 +47,17 @@ const TasksView = ({
 
   const view = hasTasks ? tasksView() : emptyStateView();
 
+  const header = (
+    <Aside.Header
+      icon={<img src={TasksIcon} alt="Feed" />}
+      title="Feed"
+      onClose={closeView}
+      className={asideHeaderStyles.asideHeader}
+    />
+  );
+
   return (
-    <Aside header={<Aside.Header title="Feed" onClose={closeView} className={asideHeaderStyles.asideHeader} />}>
+    <Aside header={header}>
       <PageView loadingState={loadingState} view={view} />
     </Aside>
   );
