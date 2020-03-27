a04822c67fd39276fad00a48430c8b8d64531eda
Matt Ensor <mattyensor@gmail.com>
2020-03-27 13:07:20 +1100

    only loading tasks when a business id has been set (#2494)
    Co-authored-by: Matt <matt.ensor@myob.com>

--- src/root/services/tasks/loadTasks.js
+++ src/root/services/tasks/loadTasks.js
@@ -3,6 +3,8 @@ import { GET_TASKS_LIST } from '../../rootIntents';
 const loadTasks = async ({ dispatcher, integration, store }) => {
   const { businessId, region } = store.getState();
 
+  if (!businessId || !region) return;
+
   try {
     const tasks = await new Promise((resolve, reject) => integration.read({
       intent: GET_TASKS_LIST,
