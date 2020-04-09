9c745064df42e52ff6d1877489cf88a839e4f085
Robert Price <robert.price@myob.com>
2020-04-09 13:01:40 +1000

    Employee Learn Copy Updates (#2577)

--- src/modules/learning/employeeLearn/LearnEmployeeModule.js
+++ src/modules/learning/employeeLearn/LearnEmployeeModule.js
@@ -15,35 +15,35 @@ export default class LearnEmployeeModule {
   onClick = (region, businessId) => {
     this.learnEmployeeCompleted();
     window.location.href = `/#/${region}/${businessId}/employee/new?mainTab=contactDetails&appcue=-M0zEQRdUJQxH8iquMVt`;
-  }
+  };
 
   render = (region, businessId) => this.setRootView(
     <LearnComponent
-      title="A handy checklist for creating employees"
+      title="Time to add the people who grow with your business"
       media={(
         <EmployeeLearnImage className={styles.learnImage} />
       )}
     >
-      <h3>Like life, payroll success is all in the preparation</h3>
-      <p>
-      You&apos;ll be entering your employee&apos;s personal,
-      tax and pay information so PAYG and super are calculated
-      correctly. Don&apos;t worry, we&apos;ll walk you through it.
-      </p>
-      <h3>Before you start, make sure you have each employee&apos;s:</h3>
+      <h3>Before you start, you will need the following documents for each employee: </h3>
+
       <ol className={styles.checklist}>
-        <li>1. Tax file number declaration form</li>
-        <li>2. Super choice form</li>
-        <li>3. Employee contract, including salary and wage details</li>
-        <li>
-            4. Any forms for variations or exemptions
-            (for example, Medicare levy variation&nbsp;declaration)
-        </li>
+        <li>Tax file number declaration form</li>
+        <li>Superannuation choice form</li>
+        <li>Employee contract which includes salary and wage details</li>
+        <li>Any variation or exemption forms such as Medicare levy variation declaration</li>
       </ol>
+
+      <p>
+        For more information on employee forms, <a
+          href="https://www.ato.gov.au/business/your-workers/employee---checklist/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >visit the ATO website.</a>
+      </p>
       <div className={baseStyles.buttonContainer}>
         <ButtonRow
           secondary={[
-            <Button type="primary" key="gotIt" onClick={() => this.onClick(region, businessId)}>Got it</Button>,
+            <Button type="primary" key="createNewEmployee" onClick={() => this.onClick(region, businessId)}>Create new employee</Button>,
           ]}
         />
       </div>
