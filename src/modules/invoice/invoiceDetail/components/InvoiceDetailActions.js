b997807538edf5d9d64b0c8efd755f985b491a1b
Thi Do <36179897+thidoo@users.noreply.github.com>
2020-04-24 10:47:59 +1000

    Add pdf and email buttons back to read only invoice layouts (#2655)

--- src/modules/invoice/invoiceDetail/components/InvoiceDetailActions.js
+++ src/modules/invoice/invoiceDetail/components/InvoiceDetailActions.js
@@ -143,6 +143,8 @@ const InvoiceDetailActions = ({
         ]}
         secondary={[
           recordPaymentButton,
+          exportPdfButton,
+          saveAndEmailButton,
         ]}
       />);
   }
