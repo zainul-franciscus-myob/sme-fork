274d0cf8632dfa72fa833b18cc72b9fa9522c8d2
Tony Tang <tsukiy0@users.noreply.github.com>
2020-06-17 16:16:55 +1000

    Revert "SMEP-1839/readonly eofy general journal (#2839)" (#2875)
    This reverts commit a62ec71519c1243d9581b610381a1c8697145929.

--- src/modules/transactionList/components/CreditsAndDebitsListTableBody.js
+++ src/modules/transactionList/components/CreditsAndDebitsListTableBody.js
@@ -1,9 +1,8 @@
-import { Table } from '@myob/myob-widgets';
+import { Table, Tooltip } from '@myob/myob-widgets';
 import { connect } from 'react-redux';
 import React from 'react';
 
 import { getTableEntries } from '../selectors/creditsAndDebitsSelectors';
-import ReferenceNumber from './ReferenceNumber';
 
 /* eslint-disable react/no-array-index-key */
 
@@ -13,16 +12,19 @@ const CreditsAndDebitsListTableBody = (props) => {
     entries,
   } = props;
 
+  const getRefEntryLink = entry => (entry.link ? (
+    <a href={entry.link}>{entry.referenceId}</a>
+  ) : (
+    <Tooltip placement="bottom" triggerContent={entry.referenceId}>
+      This transaction type can only be viewed and edited from your desktop AccountRight software
+    </Tooltip>
+  ));
 
   const rows = entries.map((entry, index) => (
     <Table.Row key={index}>
       <Table.RowItem {...tableConfig.date}>{entry.displayDate}</Table.RowItem>
       <Table.RowItem {...tableConfig.referenceId}>
-        <ReferenceNumber
-          isSystem={entry.isSystem}
-          link={entry.link}
-          referenceId={entry.referenceId}
-        />
+        {getRefEntryLink(entry)}
       </Table.RowItem>
       <Table.RowItem {...tableConfig.description}>{entry.description}</Table.RowItem>
       <Table.RowItem {...tableConfig.displayAccountName}>{entry.displayAccountName}</Table.RowItem>
