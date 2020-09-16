6b4333236515d7f437e3b8a6cfecf76d0d96c776
Waanya Yoosthaporn <39475527+waanyay@users.noreply.github.com>
2020-09-17 09:54:41 +1000

    [SMEP-2300] Customer statement: Allow customer emails to be separated by ;

--- src/modules/customerStatement/selectors/__test__/customerStatementListSelectors.test.js
+++ src/modules/customerStatement/selectors/__test__/customerStatementListSelectors.test.js
@@ -1,5 +1,7 @@
 import { getIsDownloadingPDF } from '../customerStatementListSelectors';
+import { getSendEmailContent } from '../customerStatementListIntegrationSelectors';
 import ModalType from '../../ModalType';
+import StatementType from '../../StatementType';
 
 describe('customerStatementListSelectors', () => {
   describe('getIsDownloadingPDF', () => {
@@ -49,6 +51,50 @@ describe('customerStatementListSelectors', () => {
       const actual = getIsDownloadingPDF(state);
 
       expect(actual).toBeFalsy();
+    });
+  });
+
+  describe('getSendEmailContent', () => {
+    describe('toEmail', () => {
+      const state = {
+        customerStatements: [],
+        emailModalOptions: {},
+        templateAdditionalOptions: { statementType: StatementType.INVOICE },
+      };
+
+      it('build toEmail from selected statement', () => {
+        const actual = getSendEmailContent({
+          ...state,
+          customerStatements: [
+            { payerUid: '1', email: 'a@domain.com' },
+            { payerUid: '2', email: 'b@domain.com', isSelected: true },
+            { payerUid: '3', email: 'c@domain.com', isSelected: true },
+            { payerUid: '4', isSelected: true },
+            { payerUid: '5', email: 'd@domain.com' },
+            {
+              payerUid: '6',
+              email: 'e@domain.com;f@domain.com; g@domain.com',
+              isSelected: true,
+            },
+            {
+              payerUid: '7',
+              email: 'h@domain.com; ; i@domain.com',
+              isSelected: true,
+            },
+          ],
+        });
+
+        expect(actual.toEmail).toEqual([
+          { payerUid: '2', email: 'b@domain.com' },
+          { payerUid: '3', email: 'c@domain.com' },
+          { payerUid: '4', email: '' },
+          { payerUid: '6', email: 'e@domain.com' },
+          { payerUid: '6', email: 'f@domain.com' },
+          { payerUid: '6', email: 'g@domain.com' },
+          { payerUid: '7', email: 'h@domain.com' },
+          { payerUid: '7', email: 'i@domain.com' },
+        ]);
+      });
     });
   });
 });
--- src/modules/customerStatement/selectors/customerStatementListIntegrationSelectors.js
+++ src/modules/customerStatement/selectors/customerStatementListIntegrationSelectors.js
@@ -30,16 +30,35 @@ const getSelectedTemplateOptionForEmail = (state) => {
   return selectedTemplateOption || defaultTemplateOption;
 };
 
+const getStatementEmail = (statement) => {
+  const { email: strEmail = '' } = statement;
+
+  return strEmail.split(';').reduce((emails, str = '') => {
+    const email = str.trim();
+
+    return email ? [...emails, email] : emails;
+  }, []);
+};
+
+const getToEmail = (statements) => {
+  return statements
+    .filter(({ isSelected }) => isSelected)
+    .reduce((acc, statement) => {
+      const { payerUid } = statement;
+      const emails = getStatementEmail(statement);
+      const entries = emails.length
+        ? emails.map((email) => ({ payerUid, email }))
+        : [{ payerUid, email: '' }];
+
+      return [...acc, ...entries];
+    }, []);
+};
+
 export const getSendEmailContent = (state) => ({
   ...state.templateAdditionalOptions,
   fromEmail: state.emailModalOptions.fromEmail,
   subject: state.emailModalOptions.subject,
   message: state.emailModalOptions.message,
   templateOption: getSelectedTemplateOptionForEmail(state),
-  toEmail: state.customerStatements
-    .filter(({ isSelected }) => isSelected)
-    .map(({ payerUid, email }) => ({
-      payerUid,
-      email,
-    })),
+  toEmail: getToEmail(state.customerStatements),
 });
