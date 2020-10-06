a4b2deb00531a40f61e7164312b15214ef9b9a10
Bianca Carnevale <bianca.carnevale@myob.com>
2020-10-06 17:47:02 +1100

    CLV-568 Fix rendering valid bank details when creating bill payment (#3284)
    Co-authored-by: Justin Feng <justin.feng@myob.com>

--- src/modules/billPayment/billPaymentDetail/BillPaymentDetailModule.js
+++ src/modules/billPayment/billPaymentDetail/BillPaymentDetailModule.js
@@ -69,6 +69,7 @@ export default class BillPaymentModule {
 
     if (getSupplierId(state)) {
       this.loadBillList();
+      this.loadSupplierPaymentInfo();
     }
 
     this.loadContactCombobox();
