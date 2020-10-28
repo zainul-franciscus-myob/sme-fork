4f0d50022366cb7fafbb15cf96070d5584d245d9
Max Chen <58458540+MaxAtMyob@users.noreply.github.com>
2020-10-28 16:03:52 +1100

    CLV-678 load bill payment should reset edited status (#3389)

--- src/modules/billPayment/billPaymentDetail/billPaymentDetailReducer.js
+++ src/modules/billPayment/billPaymentDetail/billPaymentDetailReducer.js
@@ -238,6 +238,7 @@ const loadBillPayment = (state, action) => ({
     ...state.remittanceAdviceDetails,
     ...action.remittanceAdviceDefaults,
   },
+  isPageEdited: false,
 });
 
 const updateHeaderOption = (state, action) => ({
