1656666cc6ed7af010ca750b9dd1e419e035fef9
Kalyani Singh <36179968+neha29myob@users.noreply.github.com>
2020-11-23 10:56:55 +1100

    Hotfix/po-list-columns-width (#3522)
    * Fixed the column size such that supplier name has more width
    
    * Fixed the column width to take entire space
    
    * Fixed the PromisedDate content to be aligned with Header
    
    * Add flex to supplier name
    
    * Added flex to supplier name
    
    * Fixed typo for unit

--- src/modules/purchaseOrder/purchaseOrderList/components/widthConfig.js
+++ src/modules/purchaseOrder/purchaseOrderList/components/widthConfig.js
@@ -7,17 +7,17 @@ const widthConfig = ({
   promisedDate,
 }) => [
   {
-    'min-width': '1160px',
+    'min-width': '768px',
     config: [
-      { columnName: dateIssued.columnName, styles: { width: '14rem' } },
-      { columnName: number.columnName, styles: { width: '14rem' } },
+      { columnName: dateIssued.columnName, styles: { width: '18rem' } },
+      { columnName: number.columnName, styles: { width: '18rem' } },
       { columnName: supplier.columnName, styles: { width: 'flex-1' } },
-      { columnName: invoiceNumber.columnName, styles: { width: '17rem' } },
+      { columnName: invoiceNumber.columnName, styles: { width: '24rem' } },
       {
         columnName: purchaseOrderAmount.columnName,
-        styles: { width: '12rem' },
+        styles: { width: '13rem' },
       },
-      { columnName: promisedDate.columnName, styles: { width: 'flex-1' } },
+      { columnName: promisedDate.columnName, styles: { width: '12rem' } },
     ],
   },
 ];
